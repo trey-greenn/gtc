5e1afc17a119d463933be7a46454e83e00aba135
trey-greenn <w.treygreen@gmail.com>
2023-10-25 08:03:28 -0700

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <>
+    <Html >
+      
+      <Head>
+      <Script src="https://code.jquery.com/jquery-3.6.0.min.js"></Script>
+      </Head >
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
+    </>
   )
 }
